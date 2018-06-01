b7e5f85d9a475f63af84a53065dfec3e5b6d75d5
shamork <shamork@163.com>
2018-06-01 13:49:21 +0800

    1.add name property 2.matcher arg change to httpRequest to allow match headers 3.async Modifier and allow Modifier return a HttpResponse which will be directly return to the client 3.support post response proc

--- src/SharpReverseProxy/ProxyRule.cs
+++ src/SharpReverseProxy/ProxyRule.cs
@@ -4,10 +4,13 @@ using System.Net.Http;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
-namespace SharpReverseProxy {
-    public class ProxyRule {
-        public Func<Uri, bool> Matcher { get; set; } = uri => false;
-        public Action<HttpRequestMessage, ClaimsPrincipal> Modifier { get; set; } = (msg, user) => { };
+namespace SharpReverseProxy
+{
+    public class ProxyRule
+    {
+        public string Name { get; set; }
+        public Func<HttpRequest, bool> Matcher { get; set; } = r => false;
+        public Func<HttpRequestMessage, ClaimsPrincipal, Task<HttpResponseMessage>> Modifier { get; set; } = null;
         public Func<HttpResponseMessage, HttpContext, Task> ResponseModifier { get; set; } = null;
         public bool PreProcessResponse { get; set; } = true;
         public bool RequiresAuthentication { get; set; }
